fa4f76837a6a057219778d3023e1fb4079abd9e3
carmine <carmine@Lenovo-PC>
2019-03-28 21:38:05 +0100

    second commit

---
+++ helloworld/src/main/java/it/carmine/prove/helloworld/Util.java
@@ -0,0 +1,13 @@
+package it.carmine.prove.helloworld;
+
+public class Util {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
