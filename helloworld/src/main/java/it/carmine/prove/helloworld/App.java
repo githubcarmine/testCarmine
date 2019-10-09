0b33565d5563c9875dc3f5b7a5279049f72c8bc7
carmine <carmine@Lenovo-PC>
2019-10-09 21:09:29 +0200

    branchnew

--- helloworld/src/main/java/it/carmine/prove/helloworld/App.java
+++ helloworld/src/main/java/it/carmine/prove/helloworld/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! ciao ciaoy" );
+        System.out.println( "Hello World! ciao ciaoy matteo" );
     }
 }
