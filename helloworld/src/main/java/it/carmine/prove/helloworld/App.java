af433dff89320bc6cd07eaa2bee1606037bd3d61
carmine <carmine@Lenovo-PC>
2019-10-14 23:57:30 +0200

    primo

--- helloworld/src/main/java/it/carmine/prove/helloworld/App.java
+++ helloworld/src/main/java/it/carmine/prove/helloworld/App.java
@@ -10,5 +10,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! ciao ciaoy matteo" );
+        System.out.println( "Hello World! ciao ciaoy matteo" );
+        
+        
+        System.out.println( "Hello World! ciao ciaoy matteo" );
     }
 }
