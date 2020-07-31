542117b2ecacbd333d2656c4c775b9573bb8d92d
yyi2096 <carmine.quitadamo@infocamere.it>
2020-07-31 12:59:39 +0200

    PROVA RESET

--- car/src/main/webapp/index.jsp
+++ car/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 <h2>Hello Worldaaaaa</h2>
+<h2>Aggiungo rigamaster</h2>
+<h2>Aggiungo riga</h2>
+<h2>Aggiungo terza riga</h2>
+<h2>Aggiungo quarta riga d carmine2</h2>
 </body>
 </html>
--- helloworld/src/main/java/it/carmine/prove/helloworld/App.java
+++ helloworld/src/main/java/it/carmine/prove/helloworld/App.java
@@ -2,12 +2,24 @@ package it.carmine.prove.helloworld;
 
 /**
  * Hello world!
- *
+ * x
+ * alfateta
+ * alfabeta
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World! ciao ciaoy matteo" );
+
+        System.out.println( "Hello World! ciao ciaoy matteo" );
+
+        
+        
+        System.out.println( "Hello World! ciao ciaoy matteo e giulio" );
     }
+    
+    public void ciao() {
+    	 System.out.println( "Hello World! ciao ciaoy matteo e giulio rrr" );
+	}
 }
