a0256358c7848363c6851487273552bbc485b157
YYI2096 <YYI2096@IC2510391.ic.intra.infocamere.it>
2026-01-16 14:41:02 +0100

    prova commit master

--- helloworld/src/main/java/it/carmine/prove/helloworld/App.java
+++ helloworld/src/main/java/it/carmine/prove/helloworld/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! ciao ciaoy matteo" );
+        System.out.println( "Hello World! ciao ciaoy carmine 2026" );
 
         System.out.println( "Hello World! ciao ciaoy matteo" );
 
